9243bb55ba026fe94a3dde277080639a25a994ed
Bela <75233+sci@users.noreply.github.com>
2025-08-06 11:30:35 +0200

    feat: support caching

---
+++ R/cache.R
@@ -0,0 +1,55 @@
+make_db_path <- function(path, format_version, extras = NULL) {
+  path |>
+    c(extras, "classification_cache", format_version, "sqlite") |>
+    str_c(collapse = ".")
+}
+
+connect <- function(db_path) {
+  con <- DBI::dbConnect(RSQLite::SQLite(), db_path)
+}
+
+disconnect <- function(con) {
+  DBI::dbDisconnect(con)
+}
+
+tidy_cache <- function(x) {
+  x |>
+    dplyr::arrange(sequence) |>
+    dplyr::distinct(sequence, .keep_all = TRUE)
+}
+
+get_cache <- function(db_path, sequences) {
+  con <- connect(db_path)
+
+  if (DBI::dbExistsTable(con, "cache")) {
+    results <-
+      dplyr::tbl(con, "cache") |>
+      dplyr::collect() |>
+      dplyr::filter(sequence %in% sequences) |>
+      tidy_cache()
+  } else {
+    results <- tibble(sequence = character())
+  }
+
+  disconnect(con)
+
+  results
+}
+
+update_cache <- function(db_path, cached, fresh) {
+  con <- connect(db_path)
+
+  if (DBI::dbExistsTable(con, "cache")) {
+    DBI::dbAppendTable(con, "cache", fresh)
+    cli::cli_alert("cache appended ({.val {nrow(cached)}}+{.val {nrow(fresh)}} row{?s} from/to {.path {db_path}})")
+  } else {
+    DBI::dbWriteTable(con, "cache", fresh)
+    cli::cli_alert("cache created ({.val {nrow(new)}} row{?s} added to {.path {db_path}})")
+  }
+
+  disconnect(con)
+
+  cached |>
+    dplyr::bind_rows(fresh) |>
+    tidy_cache()
+}
--- R/dada2_classify.R
+++ R/dada2_classify.R
@@ -1,32 +1,79 @@
-dada2_classify <- function(sequences, reference) {
-  dada2::assignTaxonomy(
-    sequences,
-    reference,
-    multithread = Sys.getenv("THREADS", "1") |> as.integer()
-  )
+as_tibble <- function(x) {
+  x |>
+    as.data.frame() |>
+    rownames_to_column("sequence") |>
+    tibble::as_tibble()
 }
 
-dada2_classify_species <- function(sequences, reference) {
-  dada2::assignSpecies(
+dada2_classify <- function(
     sequences,
     reference,
-    allowMultiple = TRUE
+    bootstrap_threshold = 50L,
+    ranks = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")) {
+  db_path <- make_db_path(reference, 1L, str_c("bootstrap_", bootstrap_threshold))
+
+  cached <- get_cache(db_path, sequences)
+  missing_sequences <- setdiff(sequences, pull(cached, sequence))
+
+  if (vec_is_empty(missing_sequences)) {
+    cli::cli_alert("cache was up-to-date ({.val {nrow(cached)}} row{?s} read from {.path {db_path}})")
+    return(cached)
+  }
+
+  fresh <- dada2::assignTaxonomy(
+    missing_sequences,
+    reference,
+    bootstrap_threshold,
+    outputBootstraps = TRUE,
+    taxLevels = ranks,
+    multithread = Sys.getenv("THREADS", "1") |> as.integer()
   )
+
+  fresh <- fresh |> map(as_tibble)
+
+  fresh <- inner_join(
+    fresh |> chuck("tax"),
+    fresh |> chuck("boot") |> dplyr::rename_with(\(x) str_c("bootstrap_", x)),
+    by = join_by(sequence == bootstrap_sequence)
+  ) |>
+    select(sequence, all_of(str_c(
+      rep(c("", "bootstrap_"), length(ranks)),
+      rep(ranks, each = 2L)
+    )))
+
+  update_cache(db_path, cached, fresh)
+}
+
+dada2_classify_species <- function(sequences, reference, allow_multiple = TRUE) {
+  db_path <- make_db_path(reference, 1L, ifelse(allow_multiple, "allow_multiple", "disallow_multiple"))
+
+  cached <- get_cache(db_path, sequences)
+  missing_sequences <- setdiff(sequences, pull(cached, sequence))
+
+  if (vec_is_empty(missing_sequences)) {
+    cli::cli_alert("cache was up-to-date ({.val {nrow(cached)}} row{?s} read from {.path {db_path}})")
+    return(cached)
+  }
+
+  fresh <-
+    dada2::assignSpecies(
+      missing_sequences,
+      reference,
+      allowMultiple = allow_multiple
+    ) |>
+    as_tibble()
+
+  update_cache(db_path, cached, fresh)
 }
 
 tidy_classification <- function(classification, classification_species, features) {
   species <-
     classification_species |>
-    as.data.frame() |>
-    rownames_to_column("sequence") |>
-    as_tibble() |>
-    mutate(exact_match = str_c(Genus, " ", Species), .keep = "unused")
+    mutate(Species_exact_match = str_c(Genus, " ", Species), .keep = "unused")
 
   classification |>
-    as.data.frame() |>
-    rownames_to_column("sequence") |>
-    as_tibble() |>
     left_join(species, by = "sequence") |>
     left_join(features, by = "sequence") |>
-    select(2L:ends_with("ID"))
+    select(2L:ends_with("ID")) |>
+    arrange(across(last_col()))
 }
